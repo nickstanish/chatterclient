68adb6f1b177823edee8912fb87d1331bcf42dcf
nstanish <nstanish@purdue.edu>
2012-10-23 21:49:37 -0400

    upgrades to styletextpane and toolbar, which will eventually be used in every tabbed chat instance

--- src/StyleToolbar.java
+++ src/StyleToolbar.java
@@ -1,16 +1,25 @@
 import java.awt.Color;
 import java.awt.FlowLayout;
-import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JSpinner;
 import javax.swing.JToggleButton;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.event.ChangeListener;
 import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.StyleConstants;
 
 
 public class StyleToolbar extends JPanel{
-	public JToggleButton boldButton, italicsButton, underlineButton;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5073819039743726973L;
+	private JToggleButton boldButton, italicsButton, underlineButton;
+	private JComboBox colorCombo, fontCombo;
+	private JSpinner fontSizeSpinner;
 	StyleToolbar(){
 		setLayout(new FlowLayout(FlowLayout.LEADING));
 		boldButton = new JToggleButton("Bold", false);
@@ -19,8 +28,13 @@ public class StyleToolbar extends JPanel{
 		add(italicsButton);
 		underlineButton = new JToggleButton("Underline", false);
 		add(underlineButton);
+		SpinnerNumberModel fontSizeModel = new SpinnerNumberModel(12, 8, 48, 2);
+		fontSizeSpinner = new JSpinner(fontSizeModel);
+		add(fontSizeSpinner);
+		String[] colors = new String[]{"black","gray","blue","green","red","yellow"};
+		colorCombo = new JComboBox(colors);
+		add(colorCombo);
 	}
-	public SimpleAttributeSet style;
 	public SimpleAttributeSet getStyle(){
 		SimpleAttributeSet style = new SimpleAttributeSet();
 		StyleConstants.setUnderline(style, underlineButton.isSelected());
@@ -29,11 +43,17 @@ public class StyleToolbar extends JPanel{
 	    // Font family
 	    StyleConstants.setFontFamily(style, "SansSerif");
 	    // Font size
-	    StyleConstants.setFontSize(style, 12);
+	    StyleConstants.setFontSize(style, Integer.parseInt(fontSizeSpinner.getValue().toString()));
 	    // Background color
 	    //StyleConstants.setBackground(style, Color.white);
 	    // Foreground color
 	    StyleConstants.setForeground(style, Color.black);
 	    return style;
+	}
+	public void addListeners(ItemListener l, ChangeListener cl){
+		boldButton.addItemListener(l);
+		italicsButton.addItemListener(l);
+		underlineButton.addItemListener(l);
+		fontSizeSpinner.addChangeListener(cl);
 	}
 }
--- src/StyledTextPane.java
+++ src/StyledTextPane.java
@@ -1,32 +1,24 @@
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.GraphicsEnvironment;
-import java.awt.Rectangle;
-import java.awt.TexturePaint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 import java.util.Random;
 
-import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.DefaultStyledDocument;
 import javax.swing.text.Document;
@@ -35,20 +27,8 @@ import javax.swing.text.StyleConstants;
 
 
 public class StyledTextPane extends JTextPane{
-	public Document doc;
-	private BufferedImage bufferedImage;
-	private TexturePaint texturePaint;
+	private Document doc;
 	StyledTextPane(){
-		File file = new File("media/background.png");
-		try{
-			bufferedImage = ImageIO.read(file);
-		    Rectangle rect = new Rectangle(0, 0, bufferedImage.getWidth(null), bufferedImage.getHeight(null));
-		    texturePaint = new TexturePaint(bufferedImage, rect);
-		    setOpaque(false);
-		}
-		catch(IOException ie){
-			System.err.println(ie);
-		}
 		init();
 		Random r = new Random();
 		for(int x = 0; x < 12; x++){
@@ -57,9 +37,11 @@ public class StyledTextPane extends JTextPane{
 		}
 		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
 		String[] x = ge.getAvailableFontFamilyNames();
+		/*
 		for(int y = 0; y < x.length; y ++){
 			append(x[y], getStyle(x[y],0));
 		}
+		*/
 	}
 	StyledTextPane(int x){
 		init();
@@ -104,52 +86,51 @@ public class StyledTextPane extends JTextPane{
 	    }
 	   
 	}
-	 public void paintComponent(Graphics g)
-	  {
-		 if(bufferedImage!= null){
-			 Graphics2D g2 = (Graphics2D) g;
-			 g2.setPaint(texturePaint);
-		 }
-	    
-	    g.fillRect(0, 0, getWidth(), getHeight());
-	    super.paintComponent(g);
-	  }
-
 	/**
 	 * @param args
 	 */
-	public static void updateStyle(){
-		box.setStyle(toolbar.getStyle());
-		box.requestFocusInWindow();
-	}
-	public static void send(){
-		pane.append(box.getText().replaceAll("\\n$", ""), box.style);
-		box.setText("");
-		box.requestFocusInWindow();
-	}
+	
 	public static StyleToolbar toolbar;
 	public static StyledTextPane box;
 	public static StyledTextPane pane;
+	private void getStyle(){
+		style = toolbar.getStyle();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		JDialog loadingDialog = new JDialog(new JFrame(), "Loading ", false);
 		loadingDialog.setPreferredSize(new Dimension(300,300));
-		JLabel label = new JLabel("Loading");
-		label.setFont(new Font("serif",Font.BOLD,32));
-		label.setForeground(Color.white);
-		loadingDialog.getContentPane().add(label);
-		loadingDialog.getContentPane().setBackground(Color.black);
+		loadingDialog.getContentPane().add(new LoadingPanel());
 		loadingDialog.pack();
 		loadingDialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		loadingDialog.setVisible(true);
-		JFrame window = new JFrame("Styled Text");
 		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		View window = new View();
+		window.pack();
+		window.setVisible(true);
+		loadingDialog.dispose();
+
+	}
+
+}
+class View extends JFrame{
+	private StyledTextPane pane, box;
+	private StyleToolbar toolbar;
+	public void updateStyle(){
+		box.setStyle(toolbar.getStyle());
+		box.requestFocusInWindow();
+	}
+	public void send(){
+		pane.append(box.getText().replaceAll("\\n$", ""), box.style);
+		box.setText("");
+		box.requestFocusInWindow();
+	}
+	public View(){		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		JPanel panel = new JPanel();
 		pane = new StyledTextPane();
 		JScrollPane scroller = new JScrollPane(pane);
-		
-		window.getContentPane().add(panel);
+		add(panel);
 		panel.add(scroller);
 		box = new StyledTextPane(0);
 		JScrollPane scroll = new JScrollPane(box);
@@ -161,20 +142,13 @@ public class StyledTextPane extends JTextPane{
 			
 		});
 		scroller.setPreferredSize(new Dimension(300,400));
-		scroll.setPreferredSize(new Dimension(300,40));
+		scroll.setPreferredSize(new Dimension(300,80));
 		panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 		toolbar = new StyleToolbar();
-		ItemListener listener = new ItemListener(){
-			public void itemStateChanged(ItemEvent e){
-				updateStyle();
-			}
-		};
 		box.addKeyListener(new KeyListener(){
-
 			@Override
 			public void keyPressed(KeyEvent k) {
 				// TODO Auto-generated method stub
-				
 			}
 
 			@Override
@@ -192,20 +166,27 @@ public class StyledTextPane extends JTextPane{
 			}
 			
 		});
-		toolbar.boldButton.addItemListener(listener);
-		toolbar.italicsButton.addItemListener(listener);
-		toolbar.underlineButton.addItemListener(listener);
+		toolbar.addListeners(new ItemListener() {
+			@Override
+			public void itemStateChanged(ItemEvent evt) {
+				updateStyle();
+			}
+
+		}, 
+		new ChangeListener(){
+			@Override
+			public void stateChanged(ChangeEvent evt) {
+				updateStyle();
+				
+			}
+			
+		});
 		panel.add(toolbar);
 		JPanel g = new JPanel(new FlowLayout(FlowLayout.TRAILING));
 		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		scroller.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		g.add(scroll);
+		panel.add(scroll);
 		g.add(button);
 		panel.add(g);
-		window.pack();
-		window.setVisible(true);
-		loadingDialog.dispose();
-
 	}
-
 }
