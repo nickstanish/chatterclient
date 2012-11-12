4ccb160cb1b2b6fb45cc3d13807281c9340577a1
nstanish <nstanish@purdue.edu>
2012-11-12 12:56:16 -0500

    made chatterpaint more impressive

--- src/ChatterPaint.java
+++ src/ChatterPaint.java
@@ -1,22 +1,58 @@
 import java.awt.BasicStroke;
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.GeneralPath;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 public class ChatterPaint extends JPanel{
+	private JButton colorButton;
+	private JSlider sizeSlider;
+	private ChatterPaintPanel panel;
 	public ChatterPaint(){
 		super();
-		ChatterPaintPanel panel = new ChatterPaintPanel();
-		panel.setPreferredSize(new Dimension(500,500));
-		add(panel);
+		setLayout(new BorderLayout());
+		panel = new ChatterPaintPanel();
+		colorButton = new JButton("Color");
+		colorButton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				panel.setColor(JColorChooser.showDialog(null, "Color", panel.getColor()));
+				
+			}
+		});
+		sizeSlider = new JSlider(SwingConstants.HORIZONTAL, 1, 30, 1);
+		sizeSlider.setMajorTickSpacing(10);
+		sizeSlider.setMinorTickSpacing(5);
+		sizeSlider.setPaintTicks(true);
+		sizeSlider.setPaintLabels(true);
+		sizeSlider.setValue((int)panel.getSizeValue());
+		sizeSlider.addChangeListener(new ChangeListener(){
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				panel.setSizeValue(sizeSlider.getValue());
+			}
+			
+		});
+		add(panel, BorderLayout.CENTER);
+		JPanel toolPanel = new JPanel();
+		toolPanel.add(colorButton);
+		toolPanel.add(sizeSlider);
+		add(toolPanel, BorderLayout.NORTH);
 	}
 	/**
 	 * @param args
@@ -33,19 +69,39 @@ public class ChatterPaint extends JPanel{
 	}
 }
 class ChatterPaintPanel extends JPanel implements MouseListener, MouseMotionListener{
+	private ArrayList<ChatterPaintShape> pathList = new ArrayList<ChatterPaintShape>();
 	private GeneralPath path = new GeneralPath();
+	private float size = 3.0f;
 	private Color color = Color.black;
+	public void setColor(Color c){
+		color = c;
+	}
+	public Color getColor(){
+		return color;
+	}
+	public float getSizeValue(){
+		return size;
+	}
+	public void setSizeValue(float size){
+		this.size = size;
+	}
 	public ChatterPaintPanel(){
 		super();
+		setDoubleBuffered(true);
 		addMouseListener(this);
 		addMouseMotionListener(this);
-		setBackground(Color.red);
 	}
 	@Override
 	public void paintComponent(Graphics g1){
+		super.paintComponent(g1);
 		Graphics2D g = (Graphics2D)g1;
-		g.setColor(Color.red);
-		g.setStroke(new BasicStroke(3.0f,BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+	    for(int i = 0; i < pathList.size(); i++){
+	    	g.setStroke(new BasicStroke(pathList.get(i).getSize(),BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
+	    	g.setColor(pathList.get(i).getColor());
+	    	g.draw(pathList.get(i).getPath());
+	    }
+	    g.setColor(color);
+	    g.setStroke(new BasicStroke(size,BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND));
 	    g.draw(path);
 	}
 
@@ -64,8 +120,10 @@ class ChatterPaintPanel extends JPanel implements MouseListener, MouseMotionList
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent arg0) {
-		
+	public void mouseReleased(MouseEvent m) {
+		pathList.add(new ChatterPaintShape(path, color, size));
+		path = new GeneralPath();
+		repaint();
 	}
 	@Override
 	public void mouseDragged(MouseEvent m) {
@@ -81,4 +139,23 @@ class ChatterPaintPanel extends JPanel implements MouseListener, MouseMotionList
 
 	
 
+}
+class ChatterPaintShape{
+	private GeneralPath path;
+	private Color color;
+	private float size;
+	public ChatterPaintShape(GeneralPath path, Color color, float size){
+		this.path = path;
+		this.size = size;
+		this.color = color;
+	}
+	public Color getColor(){
+		return color;
+	}
+	public float getSize(){
+		return size;
+	}
+	public GeneralPath getPath(){
+		return path;
+	}
 }
